9922958725a7edaa14fc80f0e2707e78a17bbea1
Barry Deeney <mxaddict@codedmaster.com>
2015-06-30 02:30:15 +0800

    :{

--- main.cpp
+++ main.cpp
@@ -27,6 +27,30 @@
 #define LOTTO_URI_PREFFIX "http://pcso-lotto-results-and-statistics.webnatin.com/"
 #define LOTTO_URI_SUFFIX  "results.asp"
 
+/**
+ * This function gives back a timestamp...
+ */
+std::string timestamp(std::string format) {
+	time_t rawtime;
+	struct tm * timeinfo;
+	char buffer[80];
+	time (&rawtime);
+	timeinfo = localtime(&rawtime);
+	strftime(buffer, 80, format.c_str(), timeinfo);
+	std::string timestamp(buffer);
+	return timestamp;
+}
+
+/**
+ * This function is for debug output...
+ */
+void debug(std::string message) {
+	std::cout << timestamp("%d-%m-%Y %I:%M:%S") << " | " << message << std::endl;
+}
+
+/**
+ * This is for quick replace of string...
+ */
 void replace(std::string& str, const std::string& from, const std::string& to) {
 	// get the first pos
 	size_t start_pos = str.find(from);
@@ -71,23 +95,23 @@ void get_results(std::string type, int color) {
 	// We need a file name...
 	std::string file_name("results/" + type + ".csv");
 
+	// Wee need a result URL...
+	std::string result_url(LOTTO_URI_PREFFIX + type + LOTTO_URI_SUFFIX);
+
 	// We need to get a handle on the file...
 	std::ofstream file(file_name);
 
 	// Check if we have opened the file...
 	if (!file.is_open()) {
-		std::cout << "Error opening \"" << file_name << "\"" << std::endl;
+		debug("Error opening \"" + file_name + "\"");
 		return;
 	}
-
-	// Added the header to the file...
-	file << "\"Game\",\"Date\",\"Result\",\"Amount\",\"Winners\"" << std::endl;
 
 	// We need a curl request instance
 	curlpp::Easy request;
 
 	// Specify the URL
-	request.setOpt(curlpp::options::Url(LOTTO_URI_PREFFIX + type + LOTTO_URI_SUFFIX));
+	request.setOpt(curlpp::options::Url(result_url));
 
 	// Specify some headers
 	std::list<std::string> headers;
@@ -95,18 +119,22 @@ void get_results(std::string type, int color) {
 	headers.push_back(HEADER_USER_AGENT);
 	request.setOpt(new curlpp::options::HttpHeader(headers));
 	request.setOpt(new curlpp::options::FollowLocation(true));
+	request.setOpt(new curlpp::options::Encoding("gzip"));
+
+	// we need a string stream...
+	std::ostringstream str_stream;
 
 	// Configure curlpp to use stream
-	std::ostringstream responseStream;
-	curlpp::options::WriteStream streamWriter(&responseStream);
-	request.setOpt(streamWriter);
+	request.setOpt(new curlpp::options::WriteStream(&str_stream));
 
 	// Collect response
 	request.perform();
-	std::string re = responseStream.str();
+
+	// Where do we store the body?
+	std::string body = str_stream.str();
 
 	// Parse HTML and create a DOM tree
-	xmlDoc* doc = htmlReadDoc((xmlChar*)re.c_str(), NULL, NULL, HTML_PARSE_RECOVER | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING);
+	xmlDoc *doc = htmlReadDoc((xmlChar*)body.c_str(), NULL, NULL, HTML_PARSE_RECOVER | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING);
 
 	// Encapsulate raw libxml document in a libxml++ wrapper
 	auto *r = xmlDocGetRootElement(doc);
@@ -119,13 +147,8 @@ void get_results(std::string type, int color) {
 	auto amounts = root->find("//table/tr[position() > 1]/td[5]/text()");
 	auto winners = root->find("//table/tr[position() > 1]/td[6]/text()");
 
-	std::cout << colors[color];
-	std::cout << "games"   << ": " << games.size()    << ", ";
-	std::cout << "dates"   << ": " << dates.size()    << ", ";
-	std::cout << "results" << ": " << results.size()  << ", ";
-	std::cout << "amounts" << ": " << amounts.size()  << ", ";
-	std::cout << "winners" << ": " << winners.size();
-	std::cout << std::endl;
+	// Added the header to the file...
+	file << "\"Game\",\"Date\",\"Result\",\"Amount\",\"Winners\"" << std::endl;
 
 	// loop the results
 	for (int i = 0; i < games.size(); ++i) {
@@ -136,10 +159,11 @@ void get_results(std::string type, int color) {
 		file << "\"" << trim((std::string) dynamic_cast<xmlpp::ContentNode*>(amounts[i])->get_content()) << "\",";
 		file << "\"" << trim((std::string) dynamic_cast<xmlpp::ContentNode*>(winners[i])->get_content()) << "\"";
 		file << std::endl;
+
+		// Add our magical DOT...
+		std::cout << colors[color];
 		std::cout << "●";
 	}
-	std::cout << colors[0];
-	std::cout << std::endl;
 
 	// House keeping. Can we come in? :P
 	delete root;
